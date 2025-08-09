6fb62056af98b03468c98933a65ce0aa2d0ca584
Michele Sciabarra <30654959+sciabarracom@users.noreply.github.com>
2025-08-09 15:09:51 +0000

    adding extension

--- setup-lovable.sh
+++ setup-lovable.sh
@@ -3,6 +3,7 @@ curl -sL bit.ly/get-ops | bash
 source ~/.bashrc
 ops -plugin https://github.com/nuvolaris/olaris-lv
 ops lv setup "$C"
+code --install-extension pgant.antonio-ops-vscode-extension@1.3.2
 if test -z "$C"
 then echo "Installed as index.html"
      echo "Use 'ops lv setup <comp>' to set up as <comp>.html"
