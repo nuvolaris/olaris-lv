b01454752bf539e69a0a812787faa09a1e27f45c
Michele Sciabarra <msciabarra@apache.org>
2025-08-20 10:43:35 +0200

    check valid folder

--- setup.py
+++ setup.py
@@ -19,65 +19,68 @@ else:
     name = c.capitalize()
 
 # fix package.json
-pj = json.loads(Path("package.json").read_text(encoding="utf-8"))
-pj["openserverless"] = {
-    "devel": "npm run dev",
-    "deploy": "npm run build"
-}
-pj["scripts"]["build"] = build
-Path("package.json").write_text(json.dumps(pj, indent=2), encoding="utf-8")
+try:
+    pj = json.loads(Path("package.json").read_text(encoding="utf-8"))
+    pj["openserverless"] = {
+        "devel": "npm run dev",
+        "deploy": "npm run build"
+    }
+    pj["scripts"]["build"] = build
+    Path("package.json").write_text(json.dumps(pj, indent=2), encoding="utf-8")
 
-# fix vite.config.js
-file = Path("vite.config.js")
-if not file.exists():
-    file = Path("vite.config.ts")
-    
-vc = file.read_text()
-if(vc.find("OPSDEV_HOST") == -1):
-    pos = vc.find("server: {\n") +10
-    vc1 = vc[:pos] + """proxy: {
-  "/api/my": {
-    target: process.env.OPSDEV_HOST,
-    changeOrigin: true
-  }
-},
-""" + vc[pos:]
-    m = "defineConfig(({ mode }) => ({\n"
-    pos = vc1.find(m) + len(m)
-    vc2 = vc1[:pos] + f"""build: {{
-    outDir: "web"
-}},
-""" +vc1[pos:]
-    vc = vc2
-    file.write_text(vc, encoding="utf-8")
+    # fix vite.config.js
+    file = Path("vite.config.js")
+    if not file.exists():
+        file = Path("vite.config.ts")
+        
+    vc = file.read_text()
+    if(vc.find("OPSDEV_HOST") == -1):
+        pos = vc.find("server: {\n") +10
+        vc1 = vc[:pos] + """proxy: {
+    "/api/my": {
+        target: process.env.OPSDEV_HOST,
+        changeOrigin: true
+    }
+    },
+    """ + vc[pos:]
+        m = "defineConfig(({ mode }) => ({\n"
+        pos = vc1.find(m) + len(m)
+        vc2 = vc1[:pos] + f"""build: {{
+        outDir: "web"
+    }},
+    """ +vc1[pos:]
+        vc = vc2
+        file.write_text(vc, encoding="utf-8")
 
-# fix router
-file = Path("src/App.jsx")
-if not file.exists():
-    file = Path("src/App.tsx")
-app = file.read_text(encoding="utf-8").replace("BrowserRouter", "HashRouter")
-file.write_text(app, encoding="utf-8")
+    # fix router
+    file = Path("src/App.jsx")
+    if not file.exists():
+        file = Path("src/App.tsx")
+    app = file.read_text(encoding="utf-8").replace("BrowserRouter", "HashRouter")
+    file.write_text(app, encoding="utf-8")
 
-# remove index if renaming
-shutil.rmtree("web", ignore_errors=True)
-shutil.rmtree("packages", ignore_errors=True)
+    # remove index if renaming
+    shutil.rmtree("web", ignore_errors=True)
+    shutil.rmtree("packages", ignore_errors=True)
 
-os.makedirs(dir, exist_ok=True)
+    os.makedirs(dir, exist_ok=True)
 
-Path(f"{dir}/__main__.py").write_text(f"""
-#--kind python:default
-#--web true
-#--annotation index '99:Lovable:{name}:admin:/{index}'
-import {c}
-def main(args):
-    return {{
-        "body": {c}.{c}(args)
-     }} 
-""")
+    Path(f"{dir}/__main__.py").write_text(f"""
+    #--kind python:default
+    #--web true
+    #--annotation index '99:Lovable:{name}:admin:/{index}'
+    import {c}
+    def main(args):
+        return {{
+            "body": {c}.{c}(args)
+        }} 
+    """)
 
-Path(f"{dir}/{c}.py").write_text(f"""
-def {c}(args):
-    return {{
-        "output": "Welcome to {name}" 
-    }} 
-""")
+    Path(f"{dir}/{c}.py").write_text(f"""
+    def {c}(args):
+        return {{
+            "output": "Welcome to {name}" 
+        }} 
+    """)
+except FileNotFoundError as e:
+    print(f"This folder does not look like a valid Lovable app... cannot continue")
