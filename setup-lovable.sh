b7c69035bf9a856dca4f6d1c046740a2f181da53
Michele Sciabarra <msciabarra@apache.org>
2025-08-21 09:23:32 +0200

    setup

--- setup-lovable.sh
+++ setup-lovable.sh
@@ -2,9 +2,10 @@
 curl -sL bit.ly/get-ops | bash
 echo "export OPS_BRANCH=main" >> ~/.bashrc
 source ~/.bashrc
-nvm use 20
 ops -plugin https://github.com/nuvolaris/olaris-lv
 ops lv setup "$C"
+code --install-extension ms-vscode.test-adapter-converter
+code --install-extension hbenl.vscode-test-explorer
 code --install-extension pgant.antonio-ops-vscode-extension@1.3.2
 if test -z "$C"
 then echo "Installed as index.html"
