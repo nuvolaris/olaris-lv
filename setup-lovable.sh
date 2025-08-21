daad2821efdce5c8a60b0353bb7db277482d3901
Michele Sciabarra <msciabarra@apache.org>
2025-08-21 10:31:26 +0200

    setup with new

--- setup-lovable.sh
+++ setup-lovable.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
+source /usr/local/share/nvm/nvm.sh
+nvm use 20
 curl -sL bit.ly/get-ops | bash
 code --install-extension ms-vscode.test-adapter-converter
 code --install-extension hbenl.vscode-test-explorer
 code --install-extension pgant.antonio-ops-vscode-extension@1.3.2
 echo "export OPS_BRANCH=main" >> ~/.bashrc
 source ~/.bashrc
-source /usr/local/share/nvm/nvm.sh
-nvm use 20
 ops -plugin https://github.com/nuvolaris/olaris-lv
 ops lv setup "$C"
-ops lv new "$C" lovable
+ops lv new "${C:-index}" lovable
 if test -z "$C"
 then echo "Installed as index.html"
      echo "Use 'ops lv setup <comp>' to set up as <comp>.html"
