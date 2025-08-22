bd63e58455e8abb86a0ca39ec2c07c140d23223a
Michele Sciabarra <msciabarra@apache.org>
2025-08-22 10:09:39 +0200

    fix

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ else:
     c = sys.argv[1]
     dir = f"packages/lovable/{c}"
     index = f"{c}.html"
-    build = f"vite build && mv web/index.html web/{index}"
+    build = f"vite build && mv web/index.html web/{index} && echo \"Deployed as $OPSDEV_HOST/{c}.html\""
     name = c.capitalize()
 
 # fix package.json
@@ -24,7 +24,7 @@ try:
     pj = json.loads(Path("package.json").read_text(encoding="utf-8"))
     pj["openserverless"] = {
         "devel": "npm run dev",
-        "deploy": f"npm run build ; echo \"Deployed as $OPSDEV_HOST/{c}.html\""
+        "deploy": f"npm run build"
     }
     pj["scripts"]["build"] = build
     Path("package.json").write_text(json.dumps(pj, indent=2), encoding="utf-8")
